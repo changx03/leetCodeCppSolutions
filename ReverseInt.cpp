2c18326ba1435efcc739178a1710d12cf33b453c
Xinglong Chang <xcha011@UoA.auckland.ac.nz>
2016-08-11 14:19:54 +1200

    modified:   ReverseInt.cpp

--- ReverseInt.cpp
+++ ReverseInt.cpp
@@ -6,17 +6,16 @@ int reverse(int x) {
     double rev = 0.0;
     while(x != 0) {
         rev = rev*10 + x%10;
+        if(rev > INT_MAX || rev < INT_MIN) {
+            return 0;
+        }
         x /= 10;
-    }
-    if(rev > INT_MAX || rev < INT_MIN) {
-        rev = 0.0;
     }
     return (int)rev;
 }
 
-
 int main(int argc, char *argv[]) {
-    int x = 1534236469;
+    int x = -2147483648;
     cout << reverse(x) << endl;
     return 0;
 }
