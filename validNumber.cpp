7ef2eec33a3920c7952fbc7433b7e6c2bf3d795f
Xinglong Chang <xcha011@UoA.auckland.ac.nz>
2016-08-11 12:13:23 +1200

    modified:   validNumber.cpp

--- validNumber.cpp
+++ validNumber.cpp
@@ -15,60 +15,66 @@ bool isNumber(string s) {
     }
     int i = 0;
     int j = s.size() - 1;
-    while(i < j){
+    while(i <= j){
         if(s[i] == ' ') i++;
         if(s[j] == ' ') j--;
         if(s[i] != ' ' && s[j] != ' ') break;
     }
-    if((s[i] == '+' || s[i] == '-') && (i < j)) i++;
-    bool isFloat = false;
-    bool anyNum = false;
-    while(i <= j) {
-        if(!isDigit(s[i])){
-            if(s[i] == '.'&& !isFloat) {
-                i++;
-                isFloat = true;
-                continue;
-            } else if((s[i] == 'e' || s[i] == 'E') && anyNum && (i<j)) {
-                if(s[i+1] == '+' || s[i+1] == '-') i = i+ 2;
-                else i++;
-                anyNum = false;
-                while(i<= j) {
-                    if(isDigit(s[i])){
-                        anyNum = true;
-                        i++;
-                    }else
-                        return false;
-                }
-            } else
-                return false;
-        } else{
-            anyNum = true;
-        }
+    if((s[i] == '+' || s[i] == '-') && (i <= j)) i++;
+    bool isNumeric = false;
+    while(i <= j && isDigit(s[i])) {
+        isNumeric = true;
         i++;
     }
-    return anyNum;
+    if((s[i] == '.') && (i <= j)) {
+        i++;
+        while(i <= j && isDigit(s[i])){
+            isNumeric = true;
+            i++;
+        }
+    }
+    if((s[i] == 'e' || s[i] == 'E') && isNumeric){
+        isNumeric = false;
+        i++;
+        if((s[i] == '+' || s[i] == '-') && (i <= j)) i++;
+        while(i <= j){
+            if(isDigit(s[i])) {
+                i++;
+                isNumeric = true;
+            } else {
+                return false;
+            }
+        }
+    }
+    while(i <= j){
+        if(isDigit(s[i])) {
+            i++;
+            isNumeric = true;
+        } else {
+            return false;
+        }
+    }
+    return isNumeric;
 }
 
 
 int main(int argc, char *argv[]) {
-    //string str = " 0 ";
-    //string str = "6e6.5";
-    //string str = " +88 ";
-    //string str = " -8A8  ";
-    //string str = "  -6.8";
-    //string str = " +.55";
-    //string str = " 9999999999999999.";
-    //string str = "0. ";
-    //string str = "0e4";
-    //string str = "0e.4";
-    //string str = " ";
-    //string str = "e";
-    //string str = ".1";
-    //string str = "46.e3";
+//     string str = " A ";
+    //    string str = " 0 ";
+    //    string str = "6e6.5";
+    //    string str = " +88 ";
+    //    string str = " -8A8  ";
+    //    string str = "  -6.8";
+//        string str = " +.55";
+//    string str = "9.";  // expected answer: True!
+//    string str = "0..";
+//    string str = "0e.4";
+//    string str = " ";
+//    string str = "e";
+//    string str = ".1";
+    string str = "46.e3";  // expected answer: True!
     //string str = " 005047e+6  ";
     //string str = " 005047e+  ";
-    string str = "6e6.5";
     bool num = isNumber(str);
     cout << str << " is digit: " << num << endl;
     return 0;
